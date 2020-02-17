68a5ae2935fcf25be6e940daa936bce75ffff1ca
Olivia Goodman <oliviagoodman@Olivias-MacBook-Air.local>
2020-02-18 10:38:39 +1300

    version 3
    cleaning up code and adding comments

--- problem 12.py
+++ problem 12.py
@@ -1,16 +1,19 @@
 #problem 12
 #olivia g 18/2/20
 #this program will calculate the number of buses needed and how many kids on average can fit per bus
+
+#imports
 import math
 
+#getting input - the bus capacity and number of children
 cap = int(input("How many passengers can each bus take? "))
 kid = int(input("How many chidlren are going on the trip? "))
 
-
+#process - calculating the buses needed (int rounded UP) and average num of kids per bus
 bus = math.ceil(kid / cap)
 avg_kid = kid / bus
 
-
+#output - all the variables
 print("--------------------")
 print("Capacity: {} passengers".format(cap))
 print("Number of children: {}".format(kid))
