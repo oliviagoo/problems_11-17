fc57d7f6c8e1692a0194d0d8fd2666b3fe32e705
Olivia Goodman <oliviagoodman@Olivias-MacBook-Air.local>
2020-02-18 10:04:07 +1300

    version 1
    working out how to get the calculation to work - how many whole bars, and the remainder (e.g. 0.3 of a bar)

--- problem 11.py
+++ problem 11.py
@@ -1,3 +1,15 @@
 #problem 11
 #olivia g 18/2/20
 #this program will split choclate bars between people
+
+
+SQUARE = 7
+
+bars = int(input("How many chocolate bars do you have? "))
+people = int(input("How many people are there to share amongst? "))
+
+whole_bar = bars // people
+print(whole_bar)
+
+extra = bars % people
+print(extra)
