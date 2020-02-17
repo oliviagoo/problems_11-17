d0a3a7f106e3eee9cde8d7a2de420ada5c9d1fb9
Olivia Goodman <oliviagoodman@Olivias-MacBook-Air.local>
2020-02-18 10:32:53 +1300

    version 1
    getting the calculation to work, figuring out the exact number of buses then rounding UP to the next integer, also the average number of kids per bus

--- problem 12.py
+++ problem 12.py
@@ -1,3 +1,17 @@
 #problem 12
 #olivia g 18/2/20
 #this program will calculate the number of buses needed and how many kids on average can fit per bus
+import math
+
+cap = int(input("How many passengers can each bus take? "))
+kid = int(input("How many chidlren are going on the trip? "))
+
+
+bus_float = kid / cap
+print(bus_float)
+
+bus_int = math.ceil(bus_float)
+print(bus_int)
+
+avg_kid = kid / bus_int
+print(avg_kid)
