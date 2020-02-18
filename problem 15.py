3d554c2182257ce6203726de8917c982c48b9e78
Olivia Goodman <oliviagoodman@Olivias-MacBook-Air.local>
2020-02-19 12:05:29 +1300

    version 2
    cleaning up and commenting

--- problem 15.py
+++ problem 15.py
@@ -2,13 +2,15 @@
 #olivia g 19/2/20
 #this program will calculate how many pages have been printed
 
-
+#getting input - sheets before jam and pages per book
 sheets = int(input("How many sheets printed before the printer jammed? "))
 page_num = int(input("How many pages are there in a book? "))
 
+#process - calculating whole books and unfinished book pages
 books = sheets // page_num
 pages = sheets % page_num
 
+#formatted output
 print("---------------")
 print("Complete books printed: {}".format(books))
 print("Unfinished book pages printed: {}".format(pages))
