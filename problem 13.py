c08b0f512780e543852dbaff9e1b596549b373a1
Olivia Goodman <oliviagoodman@Olivias-MacBook-Air.local>
2020-02-19 11:45:13 +1300

    version 1
    the basic calculations

--- problem 13.py
+++ problem 13.py
@@ -1,3 +1,13 @@
 #problem 13
 #olivia g 18/2/20
 #this program will sort out netball teams for kids who want to play
+
+
+TEAM_NUM = 9
+
+kid = int(input("How many children want to play netball? "))
+
+teams = kid // TEAM_NUM
+print(teams)
+leftover = kid % TEAM_NUM
+print(leftover)
