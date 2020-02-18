ff9098a186814368c18a01b6f2b71cf780bf5042
Olivia Goodman <oliviagoodman@Olivias-MacBook-Air.local>
2020-02-19 11:49:56 +1300

    Update problem 13.py
    cleaning up the code and commenting

--- problem 13.py
+++ problem 13.py
@@ -2,18 +2,17 @@
 #olivia g 18/2/20
 #this program will sort out netball teams for kids who want to play
 
-
+#setting constant - how many kids per team
 TEAM_NUM = 9
 
+#getting input - how many children
 kid = int(input("How many children want to play netball? "))
 
-
+#process - calculating how many kids get in a team and how many teams are needed
 teams = kid // TEAM_NUM
-#print(teams)
 leftover = kid % TEAM_NUM
-#print(leftover)
 
-
+#output - the variables in context
 print("------------------")
 print("Number of children: {}".format(kid))
 print("Number of teams: {}".format(teams))
