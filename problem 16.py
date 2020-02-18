6c1a015f774a36d0bbe431dfb2f0f49a4832c35d
Olivia Goodman <oliviagoodman@Olivias-MacBook-Air.local>
2020-02-19 12:13:41 +1300

    version 1
    basic calculation

--- problem 16.py
+++ problem 16.py
@@ -1,3 +1,16 @@
 #problem 16
 #olivia g 19/2/20
 #this program will calculate how many apples the user can buy
+
+
+CHOC_PRICE = 2.5
+APP_PRICE = 0.5
+
+
+balance = float(input("How much money do you have? "))
+num_choc = int(input("How many chocolate bars do you want to buy? "))
+
+app_money = balance - num_choc * CHOC_PRICE
+print(app_money)
+apples = app_money // APP_PRICE 
+print(apples)
