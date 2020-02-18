7fb5dc948f4996bdba910e296ec3400c905fd87d
Olivia Goodman <oliviagoodman@Olivias-MacBook-Air.local>
2020-02-19 11:57:23 +1300

    Update problem 14.py
    cleaning up the code and commenting

--- problem 14.py
+++ problem 14.py
@@ -2,17 +2,16 @@
 #olivia g 19/2/20
 #this program will convert seconds to minutes and seconds
 
-
+#constant - seconds in a minute
 SEC_MIN = 60
 
-
+#getting input - seconds
 sec =  int(input("How many seconds? "))
 
+#process - converting it to minutes and seconds
 minutes = sec // SEC_MIN
-#print(minutes)
 extra_sec = sec % SEC_MIN
-#print(extra_sec)
 
-
+#output - formatted
 print("-------------------")
 print("{} seconds is {} minutes and {} seconds".format(sec, minutes, extra_sec))
