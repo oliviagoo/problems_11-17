6b76ab1c2daf35ccd1c9e4b56f7b1e5afd8d2ef1
Olivia Goodman <oliviagoodman@Olivias-MacBook-Air.local>
2020-02-19 12:18:26 +1300

    version 2
    cleaning up, commenting, output, etc.

--- problem 16.py
+++ problem 16.py
@@ -7,10 +7,18 @@ CHOC_PRICE = 2.5
 APP_PRICE = 0.5
 
 
-balance = float(input("How much money do you have? "))
+balance = float(input("How much money do you have? $"))
 num_choc = int(input("How many chocolate bars do you want to buy? "))
 
 app_money = balance - num_choc * CHOC_PRICE
-print(app_money)
+choc_money = balance = app_money
 apples = app_money // APP_PRICE 
-print(apples)
+
+
+
+print("-------------")
+print("Total money: ${:.2f}".format(balance))
+print("Money to buy chocolate bars: ${:.2f}".format(choc_money))
+print("Chocolate bars bought: {}".format(num_choc))
+print("Money to buy apples: ${:.2f}".format(app_money))
+print("Apples bought: {:.0f}".format(apples))
